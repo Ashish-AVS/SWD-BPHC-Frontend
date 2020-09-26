25cfdc02dafafc1687bd07a7a6fe02c3d87f6a9f
= <=>
2020-09-27 00:04:05 +0530

    ADDED : lady loading for routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from 'react';
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch,} from "react-router-dom";
 
@@ -9,33 +9,31 @@ import StudentPrivateRoute from "./StudentPrivateRoute";
 import OfficialPrivateRoute from "./OfficialPrivateRoute";
 import { AuthContextProvider } from "./context/auth";
 
-// pages for this product
-// import Components from "views/Components/Components.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
-import KnowYourDept from "views/KnowYourDept/KnowYourDept";
-import LoginPage from "views/LoginPage/LoginPage.js";
-import OfficialLogin from "views/Official/Official.js";
-import Admin from "layouts/Admin.js";
-import Official from "layouts/Official";
+// import routes components here
+
+const LandingPage = lazy(() => import('./views/LandingPage/LandingPage.js'));
+const KnowYourDept = lazy(() => import('views/KnowYourDept/KnowYourDept'));
+const LoginPage = lazy(() => import('views/LoginPage/LoginPage.js'));
+const OfficialLogin = lazy(() => import('views/Official/Official.js'));
+const Admin = lazy(() => import('layouts/Admin.js'));
+const Official = lazy(() => import('layouts/Official'));
 
 export default function App(props){  
 var hist = createBrowserHistory();
 
-
-
-
 return(
         <AuthContextProvider>
             <Router history={hist} >
-              <Switch>
-                {/* <Route path="/components" component={Components}  /> */}
-                <Route path="/know-your-dept" component={KnowYourDept} />
-                <Route path="/login-page" component={LoginPage} />
-                <Route path="/official-login" component={OfficialLogin} />
-                <OfficialPrivateRoute path="/official" component={Official} />
-                <StudentPrivateRoute path="/admin" component={Admin} />
-                <Route path="/"  component={LandingPage}/>
-              </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                  <Route path="/know-your-dept" component={KnowYourDept} />
+                  <Route path="/login-page" component={LoginPage} />
+                  <Route path="/official-login" component={OfficialLogin} />
+                  <OfficialPrivateRoute path="/official" component={Official} />
+                  <StudentPrivateRoute path="/admin" component={Admin} />
+                  <Route path="/"  component={LandingPage}/>
+                </Switch>
+              </Suspense>
             </Router>
         </AuthContextProvider>
 
