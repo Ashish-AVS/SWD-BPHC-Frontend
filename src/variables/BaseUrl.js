3db7ee3d86cfe889cffcb3d58d3a531d885adc3d
Mehr29 <gauravdash29@gmail.com>
2020-10-27 19:44:41 +0530

    Adding Med policy no

--- src/variables/BaseUrl.js
+++ src/variables/BaseUrl.js
@@ -1,5 +1,5 @@
-const BaseUrl='http://swd.bits-hyderabad.ac.in';
-
+//const BaseUrl='http://swd.bits-hyderabad.ac.in';
+const BaseUrl=window.location.protocol+"//"+window.location.hostname
 module.exports={
     BaseUrl
 };
--- src/views/MedClaim/Medclaim.js
+++ src/views/MedClaim/Medclaim.js
@@ -8,6 +8,8 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 import Button from "components/CustomButtons/Button.js";
+import BaseUrl from "variables/BaseUrl";
+import { set } from "d3";
 const styles = {
   typo: {
     paddingLeft: "25%",
@@ -48,7 +50,31 @@ const styles = {
 const useStyles = makeStyles(styles);
 
 export default function Medclaim() {
+  const [polNo,setPolNo]=React.useState("");
   const classes = useStyles();
+  const token = JSON.stringify(localStorage.getItem("tokens"));
+  const medData=React.useState({})
+  React.useEffect(()=>{
+    try{
+    const fetchData1= async ()=>{
+      const result= await fetch(`${BaseUrl}/api/ll/insurance`,{
+        headers:{Authorization:token}
+      }) ;
+      const res = await result.json();
+      if(res.err===false){
+      setPolNo(res.data.pol_no);
+      }
+      else if(res.err===true) {
+          alert(res.msg)
+      }  
+  }
+    fetchData1();
+    
+  }catch(err){
+      console.log(err);
+    }
+  
+  },[token]);
   return (
       <div>
         <div className={classes.typo} style={{marginTop:"-50px"}}>
@@ -61,16 +87,16 @@ export default function Medclaim() {
       </CardHeader>
       <CardBody>
       <h3 style={{display:"flex",justifyContent:"center"}}><b>POLICY DETAILS OF INSTITUTE</b></h3>
-      <p><b>Policy Number:</b>4211-200101-19-7000661-00-001<br/>
-		<b>Insured Name:</b> M/S Birla Institute of Technology &amp; Science Pilani Hyderabad Campus<br/>
-		<b>Period of Insurance:</b> From 00:01 Hrs of 03/09/2019 To Midnight 23:59 Hrs of 02/09/2020<br/><br/>
+      <p><b>Policy Number:</b>51-20-00561-00-00<br/>
+		<b>Insured Name:</b> M/s Birla Institute of Technology and Science Pilani Hyd<br/>
+		<b>Period of Insurance:</b> From 00:01 Hrs of 03/09/2020 To Midnight 23:59 Hrs of 02/09/2021<br/><br/>
 
 		<b>Name:</b> Raju Kumar, ADITYA BIRLA INSURANCE BROKERS LTD<br/>
 		<b>Email:</b> <a href="mailto:raju.r.kumar@adityabirlacapital.com">raju.r.kumar@adityabirlacapital.com</a><br/>
 		<b>Mobile Number:</b>+91 8510006711<br/><br/></p>
-		<b><p>For Group Mediclaim Policy Ready Reckoner of BITS Pilani Hyderabad Campus for AY 2019 - 20 :</p></b><Button round color="info">Click Here</Button><br/><br/>
+		<b><p>For Group Mediclaim Policy Ready Reckoner of BITS Pilani Hyderabad Campus for AY 2020 - 21 :</p></b><Button disabled round color="info">Click Here</Button><br/><br/>
 		<p><b>For Policy related assistance:</b> Contact Mr. Prasant Inturi on 9010202837 or Visit SWD office</p>
-		<b><p>For more details:</p></b><Button round color="info">Click Here</Button><br/>
+		<b><p>For more details:</p></b><Button disabled round color="info">Click Here</Button><br/>
 		<center><p><b>OR</b></p></center>
 		<p>Please feel free to write to them on <b><a href="mailto:care@libertyvideocon.com?subject=Query Regarding Medical Insurance Policy">care@libertyvideocon.com</a></b></p>
 		<center><p><b>OR</b></p></center>
@@ -78,12 +104,11 @@ export default function Medclaim() {
 
 
 		<h3 style={{display:"flex",justifyContent:"center"}}><b>POLICY DETAILS OF STUDENT</b></h3>
-		<p><b>Policy Number:</b> 4211-200101-19-7000661-00-001</p>
-		<p><b>Membership ID:</b> XXXX XXXX XXXX</p>
-		<p><b>Medical assistance number:</b> XXXXXXXXXX </p>
+		<p><b>Policy Number:</b> 51-20-00561-00-00</p>
+  <p><b>Membership ID:</b> {polNo}</p>
       </CardBody>
       <CardFooter>
-          <Button round color="info"> Download PDF</Button>
+          <Button round color="info" disabled> Download PDF</Button>
       </CardFooter>
     </Card>
   </div>
