34d3e948aa5d3b8536083c13b5679b9a5bd281d7
Ashish <avsashisha8@gmail.com>
2021-11-19 11:41:11 +0530

    Add Frontend of Mess Report

--- src/OfficialRoutes.js
+++ src/OfficialRoutes.js
@@ -7,19 +7,19 @@
 */
 // @material-ui/icons
 
-import SearchIcon from '@material-ui/icons/Search';
-import FastfoodIcon from '@material-ui/icons/Fastfood';
-import FastForwardIcon from '@material-ui/icons/FastForward';
-import FastRewindIcon from '@material-ui/icons/FastRewind';
-import AirplanemodeActiveIcon from '@material-ui/icons/AirplanemodeActive';
-import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
-import BookIcon from '@material-ui/icons/Book';
-import CardTravelIcon from '@material-ui/icons/CardTravel';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import BlockIcon from '@material-ui/icons/Block';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import DescriptionIcon from '@material-ui/icons/Description';
-import HowToRegIcon from '@material-ui/icons/HowToReg';
+import SearchIcon from "@material-ui/icons/Search";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import FastForwardIcon from "@material-ui/icons/FastForward";
+import FastRewindIcon from "@material-ui/icons/FastRewind";
+import AirplanemodeActiveIcon from "@material-ui/icons/AirplanemodeActive";
+import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
+import BookIcon from "@material-ui/icons/Book";
+import CardTravelIcon from "@material-ui/icons/CardTravel";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import BlockIcon from "@material-ui/icons/Block";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
+import DescriptionIcon from "@material-ui/icons/Description";
+import HowToRegIcon from "@material-ui/icons/HowToReg";
 // import FoodBankIcon from '@material-ui/icons/FoodBank';
 
 // core components/views for Official layout
@@ -28,149 +28,157 @@ import Search from "views/OfficialComponents/Search/Search";
 import Entry from "views/OfficialComponents/Maingate/MaingateEntry";
 import MessMenu from "views/OfficialComponents/Mess/MessMenu";
 import MessGrace from "views/OfficialComponents/Mess/MessGrace";
+import MessReport from "views/OfficialComponents/Mess/MessReport";
 import Exit from "views/OfficialComponents/Maingate/MaingateExit";
 import Outstation from "views/OfficialComponents/Outstation/Outstation";
-import Mcn from 'views/OfficialComponents/Mcn/Mcn';
-import Saf from 'views/OfficialComponents/Saf/Saf';
+import Mcn from "views/OfficialComponents/Mcn/Mcn";
+import Saf from "views/OfficialComponents/Saf/Saf";
 import GoodieExport from "views/OfficialComponents/GoodiesExport/GoodiesExport";
 import Counsellor from "views/OfficialComponents/Counsellor/Counsellor";
 import Blacklist from "views/OfficialComponents/Blacklist/Blacklist";
 import Blocklist from "views/OfficialComponents/Blocklist/Blocklist";
 import DocGen from "views/OfficialComponents/DocGen/DocGen";
 import Leave from "views/OfficialComponents/Leave/Leave";
-import MessLog from 'views/OfficialComponents/MessLog/MessLog';
-import MessReg from 'views/OfficialComponents/MessReg/MessReg';
+import MessLog from "views/OfficialComponents/MessLog/MessLog";
+import MessReg from "views/OfficialComponents/MessReg/MessReg";
 
 const dashboardRoutes = [
-  
   {
     path: "/search",
-    id:"search",
+    id: "search",
     name: "Search",
     icon: SearchIcon,
     component: Search,
-    layout: "/official"
+    layout: "/official",
   },
   {
     path: "/mess/menu",
-    id:"messmenu",
+    id: "messmenu",
     name: "Mess Menu",
     icon: FastfoodIcon,
     component: MessMenu,
-    layout: "/official"
+    layout: "/official",
+  },
+  {
+    path: "/mess/report",
+    id: "messlog",
+    name: "Mess Report",
+    icon: FastfoodIcon,
+    component: MessReport,
+    layout: "/official",
   },
   {
     path: "/maingate/entry",
-    id:"maingate",
+    id: "maingate",
     name: "Gate Entry",
     icon: FastForwardIcon,
-    component: Entry ,
-    layout: "/official"
+    component: Entry,
+    layout: "/official",
   },
   {
     path: "/maingate/exit",
-    id:"maingate",
+    id: "maingate",
     name: "Gate Exit",
     icon: FastRewindIcon,
-    component: Exit ,
-    layout: "/official"
+    component: Exit,
+    layout: "/official",
   },
   {
     path: "/outstation",
-    id:"outstation",
+    id: "outstation",
     name: "Outstation Request",
     icon: AirplanemodeActiveIcon,
-    component: Outstation ,
-    layout: "/official"
+    component: Outstation,
+    layout: "/official",
   },
   {
     path: "/mcn",
-    id:"mcn",
+    id: "mcn",
     name: "MCN Portal",
     icon: AccountBalanceWalletIcon,
-    component: Mcn ,
-    layout: "/official"
+    component: Mcn,
+    layout: "/official",
   },
   {
     path: "/saf",
-    id:"saf",
+    id: "saf",
     name: "SAF Portal",
     icon: AccountBalanceWalletIcon,
-    component: Saf ,
-    layout: "/official"
+    component: Saf,
+    layout: "/official",
   },
   {
     path: "/mess/grace",
-    id:"messgrace",
+    id: "messgrace",
     name: "Mess Graces",
     icon: BookIcon,
     component: MessGrace,
-    layout: "/official"
+    layout: "/official",
   },
   {
     path: "/goodies",
-    id:"goodies",
+    id: "goodies",
     name: "Goodies Data",
     icon: CardTravelIcon,
     component: GoodieExport,
-    layout: "/official"
+    layout: "/official",
   },
   {
     path: "/counsellor",
-    id:"counsellor",
+    id: "counsellor",
     name: "Counsellor",
     icon: EventNoteIcon,
     component: Counsellor,
-    layout: "/official"
+    layout: "/official",
   },
   {
     path: "/blacklist",
-    id:"blacklist",
+    id: "blacklist",
     name: "Blacklist Action ",
     icon: ErrorOutlineIcon,
     component: Blacklist,
-    layout: "/official"
+    layout: "/official",
   },
   {
     path: "/blocklist",
-    id:"blocklist",
+    id: "blocklist",
     name: "Blocklist Action ",
     icon: BlockIcon,
     component: Blocklist,
-    layout: "/official"
+    layout: "/official",
   },
   {
     path: "/docgen",
-    id:"doc",
+    id: "doc",
     name: "Document Generation ",
     icon: DescriptionIcon,
     component: DocGen,
-    layout: "/official"
+    layout: "/official",
   },
   {
     path: "/leave",
-    id:"emergency-leave",
+    id: "emergency-leave",
     name: "On Campus",
     icon: DescriptionIcon,
     component: Leave,
-    layout: "/official"
+    layout: "/official",
   },
   {
-    path: '/messlog',
-    id: 'messlog',
-    name: 'Mess Log',
+    path: "/messlog",
+    id: "messlog",
+    name: "Mess Log",
     icon: DescriptionIcon,
     component: MessLog,
-    layout: "/official"
+    layout: "/official",
   },
   {
-    path: '/messreg',
-    id: 'messreg',
-    name: 'Mess Registration',
+    path: "/messreg",
+    id: "messreg",
+    name: "Mess Registration",
     icon: HowToRegIcon,
     component: MessReg,
-    layout: "/official"
-  }
+    layout: "/official",
+  },
 
   /*
   {
@@ -208,7 +216,6 @@ const dashboardRoutes = [
     component: RTLPage,
     layout: "/rtl"
   }*/
- 
 ];
 
 export default dashboardRoutes;
---
+++ src/views/OfficialComponents/Mess/MessReport.js
@@ -0,0 +1,342 @@
+import React from "react";
+import Datetime from "react-datetime";
+import {Redirect} from 'react-router-dom';
+import {saveAs} from 'file-saver';
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+//Auth Components
+import { useAuth } from "context/auth";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from "@material-ui/core/InputLabel";
+import {BaseUrl} from "variables/BaseUrl";
+import {
+  primaryColor,
+  defaultFont
+} from "assets/jss/material-kit-react.js";
+
+
+const styles = {
+  cardCategoryWhite: {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+  typo: {
+    paddingLeft: "20%",
+    marginBottom: "30px",
+    position: "relative",
+    
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none"
+  },
+  input: {
+    color: "#495057",
+    height: "unset",
+    "&,&::placeholder": {
+      fontSize: "14px",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      fontWeight: "400",
+      lineHeight: "1.42857",
+      opacity: "1"
+    },
+    "&::placeholder": {
+      color: "#AAAAAA"
+    }
+  },
+  formControl: {
+    margin: "0 0 17px 0",
+    paddingTop: "27px",
+    position: "relative",
+    "& svg,& .fab,& .far,& .fal,& .fas,& .material-icons": {
+      color: "#495057"
+    }
+  },
+  underline: {
+    "&:hover:not($disabled):before,&:before": {
+      borderColor: "#D2D2D2 !important",
+      borderWidth: "1px !important"
+    },
+    "&:after": {
+      borderColor: primaryColor
+    }
+  },
+  label: {
+    color: "rgba(0, 0, 0, 0.26)",
+    top:"-17px",
+    fontSize: "14px",
+    transition: "0.3s ease all",
+    lineHeight: "1.428571429",
+    fontWeight: "400",
+    paddingLeft: "0",
+    letterSpacing: "normal",
+    "& + $underline": {
+      marginTop: "0px"
+    }
+  },
+  labelRoot: {
+    ...defaultFont,
+    color: "#AAAAAA !important",
+    fontWeight: "400",
+    fontSize: "14px",
+    lineHeight: "1.42857",
+    top: "10px",
+    letterSpacing: "unset",
+    "& + $underline": {
+      marginTop: "0px"
+    }}
+};
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles(styles);
+
+export default function MessReport() {
+
+ const token=JSON.parse(localStorage.getItem("officialtokens"));
+ 
+  //const [sendingData,setSendingData]=React.useState(false);
+  const [reportData,setReportData]=React.useState([]);
+  const [sendingData,setSendingData]=React.useState(false);
+  const [postDate,setPostDate]=React.useState("");
+  const [success,setSuccess]=React.useState(false);
+  const {onOfficialLogin}=useAuth();
+  const classes = useStyles();
+  const [err,setErr]=React.useState(false);
+  const [errMsg,setErrMsg]=React.useState('');
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setErr(false);
+    setSuccess(false);
+  };
+ const date = new Date();
+ const date1= date.toLocaleDateString();
+  React.useEffect(()=>{
+  
+    //setRecievedData(false);
+    try{
+      const SendData=async ()=>{
+        const result =await fetch(`${BaseUrl}/api/o/messgrace/upcoming`,{
+          headers:{Authorization:`Bearer ${token}`}
+        })
+         const res = await result.json();
+        if (res.err === false) {
+          setReportData(res.data);
+          //setRecievedData(true);
+          //setSendingData(false);
+        }
+        else if (res.err === true && result.status === 401) {
+          logout();
+        }
+        else if (res.err === true) {
+          setErr(true);
+          setErrMsg(res.msg);
+        }}
+      SendData();
+      
+    }
+    catch(err){
+      console.log(err);
+      
+    } 
+  
+  },[])
+  React.useEffect(()=>{
+  
+    if(sendingData===true){
+    try{
+      const SendData=async ()=>{
+        const result =await fetch(`${BaseUrl}/api/o/messgrace/export?date=${postDate}`,{
+          headers:{Authorization:`Bearer ${token}`,
+          Accept: "text/csv"}
+        })
+        const res= await result.text();
+        if(result.status===201||result.status===200||result.status===304){
+          const csvBlob=new Blob([res],{type:'text/csv'});
+          saveAs(csvBlob,`GraceData-${date1}.csv`);
+          setSuccess(true);
+        }
+        else {
+          setErr(true);
+          setErrMsg('Error in Downloading!');
+        }
+        }
+      SendData();
+      setSendingData(false);
+      
+    }
+    catch(err){
+      console.log(err);
+      
+    }
+  }
+  
+  })
+
+ /* React.useEffect(()=>{
+   if(messUpdate===true){
+    try{
+      const sendData=async ()=>{
+        const result =await fetch(`${BaseUrl}/api/o/messmenu`,{
+            method:"post",
+            headers:{'Content-Type':"application/json",
+            Authorization:`Bearer ${token}`},
+            body:JSON.stringify({
+              messno:1,
+              menu:JSON.stringify(messMenuData)
+            })
+           })
+         const res = await result.json();
+        if(result.status===200||result.status===201){
+          console.log("hi");
+          setMenuUpdated(true);
+          setMessUpdate(false);
+        }
+      
+    }
+      sendData();
+      
+    }
+    catch(err){
+      console.log(err);
+      
+    } 
+  }
+  },[messUpdate,token,messMenuData])
+  
+  */
+ const logout=()=>{
+  localStorage.removeItem("officialtokens");
+  onOfficialLogin(false);  
+  return (<Redirect exact to='/' />);
+}
+  return (
+    <div>
+      <div className={classes.typo} style={{marginTop:"-50px"}}>
+          <h2><strong>BITS PILANI , HYDERABAD CAMPUS</strong></h2>
+      </div>
+      <GridContainer justify="center" alignItems="center">
+        <GridItem xs={12} sm={12} md={10}>
+          <Card>
+            <CardHeader color="primary">
+              <h4 className={classes.cardTitleWhite}><b>MESS REPORT</b></h4>
+              
+            </CardHeader>
+            <CardBody>
+            <h3 style={{display:"flex",justifyContent:"center"}}><b>Download Report</b></h3>
+            
+              <GridContainer  direction="row"  justify="center"  alignItems="center">
+                  <GridItem xs={12} sm={12} md={4}>
+                  <FormControl fullWidth className={classes.formControl}>
+                  <InputLabel className={classes.label}>
+                    Select Date
+                  </InputLabel>
+                  <Datetime
+                   dateFormat="DD-MM-YYYY" 
+                   timeFormat={false}
+                   className={classes.input+" "+classes.underline}
+                   
+                   onChange={(e)=>{
+                    const date = new Date(`${e}`);
+                    const {Date1,Month,Year}={
+                      Date1:date.getDate(),
+                      Month:date.getMonth()+1,
+                      Year:date.getFullYear()
+                      }
+                      if(Month>9){
+                        if(Date1<10){
+                      setPostDate(`${Year}-${Month}-0${Date1}`);}
+                      else
+                      setPostDate(`${Year}-${Month}-${Date1}`);
+                      }else{
+                        if(Date1<10)
+                        setPostDate(`${Year}-0${Month}-0${Date1}`);
+                        else
+                        setPostDate(`${Year}-0${Month}-${Date1}`);
+                      }  
+                     }}
+                    />
+                </FormControl>
+                
+              </GridItem>
+                <GridItem xs={12} sm={12} md={4}>
+                
+                    <Button color="success" onClick={()=>{
+                      setSendingData(true)
+                      }}>
+                      Download CSV
+                    </Button>
+                  
+                </GridItem>
+                
+              </GridContainer> 
+              <h3 style={{display:"flex",justifyContent:"center",marginTop:'40px'}}><b>Meal Count</b></h3>             
+             <GridContainer spacing={2} justify="center" alignItems="center">
+               <GridItem xs={12} sm={12} md={3}>
+                 <h4><b>Breakfast</b></h4><br/>
+                    <h4>{reportData.breakfast}</h4>
+               </GridItem>
+               <GridItem xs={12} sm={12} md={3}>
+               <h4><b>Lunch</b></h4><br/>
+                 <h4>{reportData.lunch}</h4>
+               </GridItem>
+               <GridItem xs={12} sm={12} md={3}>
+               <h4><b>Snacks </b></h4><br/>
+                    <h4>{reportData.snacks}</h4>
+               </GridItem>
+               <GridItem xs={12} sm={12} md={3}>
+               <h4><b>Dinner </b></h4><br/>
+                    <h4>{reportData.dinner}</h4>
+               </GridItem>
+             </GridContainer>
+            </CardBody>
+          </Card>
+        </GridItem>
+       
+      </GridContainer>
+      <Snackbar
+           anchorOrigin={{horizontal:'left',vertical:'bottom'}}
+            open={success}
+            autoHideDuration={4000}
+            onClose={handleClose}>
+            <Alert
+              onClose={handleClose}
+              severity="success">
+              File Download completed
+        </Alert>
+          </Snackbar>
+          <Snackbar
+           anchorOrigin={{horizontal:'left',vertical:'bottom'}}
+            open={err}
+            autoHideDuration={4000}
+            onClose={handleClose}>
+            <Alert
+              onClose={handleClose}
+              severity="error">
+              {errMsg}
+        </Alert>
+          </Snackbar>
+    </div>
+  );
+}
